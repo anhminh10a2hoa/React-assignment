309b2c81d13c9e694c4b07ed34eba8cb15a206cb
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-10-22 01:20:02 +0300

    ... update ...

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -4,43 +4,23 @@ import axios from 'axios';
 
 const User = () => {
   let {id}: any = useParams();
-  let a: string = "";
   const [user, setUser] = useState({} as any);
   const [loading, setLoading] = useState(true);
   
   useEffect(() => {
     axios
       .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((response) => setUser(response.data))
+      .then((response) => setUser(JSON.stringify(response.data)))
       .then(() => setLoading(false))
     }, [setUser]
   );
 
-  for(let info in user){
-    if(info == 'address'){
-      for(let adr in user.address){
-        if(adr !== 'geo'){
-          a += `${adr}: ${user.address[adr]} \n`;
-        }
-      }
-    }
-    else if(info == 'company'){
-      for(let cpn in user.company){
-        if(cpn == 'name'){
-          a += `Company name: ${user.company[cpn]} \n`;
-        }
-      }
-    }
-    else {
-      a += `${info}: ${user[info]} \n`;
-    }
-  }
   return(
     <React.Fragment>
     {loading ? (<p>It's loading...</p>) : (
       <div >
-        {a.split('\n').map((str, i) => <p key={i}>{str}</p>)}
-        <a href="/">Return</a>
+        {user.split(',').join('{').split('{').join('}').split('}').map((str: string, i: number) => <p key={i}>{str.replace('"', '').replace('"', '').replace('"', '').replace('"', '').replace(':', ' : ')}</p>)}
+        <a style={{paddingBottom: '50px'}} href="/">Return</a>
     </div>
     )}
     </React.Fragment>
